1aea8247dce2f3fbb522e539ecf3451a6a4759c2
Dan Connolly <dconnolly@kumc.edu>
2017-04-06 12:32:27 -0500

    code_to_data: indent tests conventionally

--- code_to_data/code_to_data.py
+++ code_to_data/code_to_data.py
@@ -2,46 +2,46 @@
 
 Get codes used in glucose_concepts SQL view:
 
->>> glucose_codes = Table1Script.glucose_codes()
+    >>> glucose_codes = Table1Script.glucose_codes()
 
 
 Get all LOINC codes from the babel audit:
 
->>> from lab_sheet_normalize import BabelAudit
->>> lab_terms = BabelAudit.normal_form()
->>> site_codes = set((t.c_basecode or '')[len('LOINC:'):] for t in lab_terms)
+    >>> from lab_sheet_normalize import BabelAudit
+    >>> lab_terms = BabelAudit.normal_form()
+    >>> site_codes = set((t.c_basecode or '')[len('LOINC:'):] for t in lab_terms)
 
 
 Compare SQL code to babel audit data:
 
->>> sorted(glucose_codes['loinc_random_glucose'] - site_codes)
-[]
+    >>> sorted(glucose_codes['loinc_random_glucose'] - site_codes)
+    []
 
->>> sorted(glucose_codes['loinc_fasting_glucose'] - site_codes)
-[]
+    >>> sorted(glucose_codes['loinc_fasting_glucose'] - site_codes)
+    []
 
 Compare recent comments to babel audit data:
 (comments from https://informatics.gpcnetwork.org/trac/Project/ticket/551)
 
->>> set(LabReview.fasting_glucose_loinc) - site_codes
-set()
+    >>> set(LabReview.fasting_glucose_loinc) - site_codes
+    set()
 
->>> sorted(set(LabReview.random_glucose_loinc) - site_codes)
-['50673-3', '55399-0', 'LP31895-3']
+    >>> sorted(set(LabReview.random_glucose_loinc) - site_codes)
+    ['50673-3', '55399-0', 'LP31895-3']
 
 
 Compare recent comments to SQL code:
 
->>> set(LabReview.fasting_glucose_loinc) - glucose_codes['loinc_fasting_glucose']  # noqa
-{'1555-2'}
+    >>> set(LabReview.fasting_glucose_loinc) - glucose_codes['loinc_fasting_glucose']  # noqa
+    {'1555-2'}
 
->>> sorted(set(LabReview.random_glucose_loinc) -
-...        set(glucose_codes['loinc_random_glucose']))
-... # doctest: +NORMALIZE_WHITESPACE
-['12610-2', '12646-6', '1504-0', '1506-5', '1517-2', '1530-5',
- '20436-2', '2339-0:N', '2339-0:T', '2342-4', '26779-9',
- '50586-7', '50587-5', '50588-3', '50589-1', '50608-9',
- '50673-3', '55399-0', '81637-1', 'LP31895-3', 'LP42107-0']
+    >>> sorted(set(LabReview.random_glucose_loinc) -
+    ...        set(glucose_codes['loinc_random_glucose']))
+    ... # doctest: +NORMALIZE_WHITESPACE
+    ['12610-2', '12646-6', '1504-0', '1506-5', '1517-2', '1530-5',
+     '20436-2', '2339-0:N', '2339-0:T', '2342-4', '26779-9',
+     '50586-7', '50587-5', '50588-3', '50589-1', '50608-9',
+     '50673-3', '55399-0', '81637-1', 'LP31895-3', 'LP42107-0']
 
 Medications
 ===========
